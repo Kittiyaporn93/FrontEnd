0cae5d6ac27e155648600bc2a8dcedf281af23b8
Kittiyaporn <6406021620157@fitm.kmutnb.ac.th>
2024-02-12 10:19:08 +0700

    save

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var bodyParser = require("body-parser");
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-const base_url = "http://10.104.16.36:3000";
-//const base_url = "http://node41091-noderest.proen.app.ruk-com.cloud";
+//const base_url = "http://10.104.16.36:3000";
+const base_url = "node56489-kitti.proen.app.ruk-com.cloud:11554";
 
 // Set the template engine
-//app.set("views", path.join(__dirname, "/public/views"));
+app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
